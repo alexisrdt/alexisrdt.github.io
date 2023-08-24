e138f51e08485cc29910ced3af22a5f8450d5204
alexisrdt <alexis.rdt@gmail.com>
2023-08-25 00:00:23 +0200

    Add base content and style

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 "use strict";
 
-const main = document.getElementsByTagName("main")[0];
-
-const header = document.createElement("header");
+// Insert header
 fetch("./header.html")
 	.then(response => response.text())
-	.then(text => header.innerHTML = text)
-	.then(() => main.insertBefore(header, main.firstChild));
+	.then(headerCode => document.body.insertAdjacentHTML("afterbegin", headerCode));
+
+// Insert footer
+fetch("./footer.html")
+	.then(response => response.text())
+	.then(footerCode => document.body.insertAdjacentHTML("beforeend", footerCode));
+
+// Intersection observer for animations
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("fade-in");
+		}
+	});
+});
+document.querySelectorAll("h1").forEach(h1 => observer.observe(h1));
